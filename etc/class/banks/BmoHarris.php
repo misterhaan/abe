26f102ca52b26ae98f67bdf560998e66ae637b71
misterhaan <misterhaan@users.noreply.github.com>
2019-01-21 23:05:34 -0600

    take csv from the new bmoharris.com

--- etc/class/banks/BmoHarris.php
+++ etc/class/banks/BmoHarris.php
@@ -5,6 +5,65 @@
  */
 class BmoHarris extends abeBank {
 	/**
+	 * Parse transactions from a CSV file for an account from BMO Harris Bank.
+	 * @param string $filename Full path to the CSV file on the server.
+	 * @param int $acctid Account ID for duplicate checking.
+	 * @return array Parsed contents of the file, or false if unable to parse.
+	 */
+	public static function ParseCsvTransactions($filename, $acctid) {
+		global $db;
+		if(false !== $fh = fopen($filename, 'r')) {
+			// first three lines are header
+			fgets($fh);
+			fgets($fh);
+			fgets($fh);
+
+			$preview = new stdClass();
+			$preview->transactions = [];
+			$preview->net = 0;
+			$preview->dupeCount = 0;
+
+			if($chkdupe = $db->prepare('select IsDuplicateTransaction(?, ?, ?, ?)'))
+				if($chkdupe->bind_param('isds', $acctid, $extid, $amount, $posted)) {
+					while($line = fgetcsv($fh)) {
+						$tran = new stdClass();
+						// translate the data
+						// first column is blank
+						$tran->posted = $posted = date('Y-m-d', strtotime($line[1]));
+						$tran->name = self::TitleCase($line[2]);
+						$amount = +$line[3];
+						// fifth column is currency which is probably always USD
+						if($line[5])
+							$tran->name .= ' ' . $line[5];
+						$tran->extid = $extid = $line[6];
+						// eigth column is credit/check/debit
+						if(strtolower($line[8]) == 'debit')
+							$amount = -$amount;
+						$tran->amount = $amount;
+
+						// fields not provided
+						$tran->transdate = null;
+						$tran->city = null;
+						$tran->state = null;
+						$tran->zip = null;
+						$tran->notes = '';
+
+						if($chkdupe->execute())
+							if($chkdupe->bind_result($dupe))
+								if($chkdupe->fetch())
+									if($tran->duplicate = $dupe)
+										$preview->dupeCount++;
+
+						$preview->net += $tran->amount;
+						$preview->transactions[] = $tran;
+					}
+					return $preview;
+				}
+		}
+		return false;
+	}
+
+	/**
 	 * Parse transactions from an OFX file for an account from BMO Harris Bank.
 	 * @param string $filename Full path to the OFX file on the server.
 	 * @param int $acctid Account ID for duplicate checking.
