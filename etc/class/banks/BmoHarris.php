d8ec04bc42b9a876db5f92c46712cbb056aa0ebc
misterhaan <misterhaan@users.noreply.github.com>
2019-01-21 23:06:10 -0600

    remove unused function

--- etc/class/banks/BmoHarris.php
+++ etc/class/banks/BmoHarris.php
@@ -159,54 +159,5 @@ class BmoHarris extends abeBank {
 		// The qfx file is basically the same as ofx, just with some extra stuff we will ignore anyway.
 		self::ParseOfxTransactions($filename, $account);
 	}
-
-	/**
-	 * Import transactions from a CSV file into an account from BMO Harris Bank.
-	 * Currently leaves out data Abe needs to know.
-	 * @param string $filename Full path to the CSV file on the server.
-	 * @param integer $account ID of the account the transactions belong to.
-	 */
-	public function ImportCsvTransactionsBroken($filename, $account) {
-		// can's use CSV because it leaves out important data
-		global $ajax, $db;
-
-		if(false !== $fh = fopen($filename, 'r')) {
-			// first three lines are headers
-			for($i = 0; $i < 3; $i++)
-				fgets($fh);
-
-				// transaction makes all these inserts much faster
-			$db->real_query('start transaction');
-
-			// prepare and bind a statement
-			if(false !== $ins = $db->prepare('insert into transactions (account, extid, posted, name, amount) values (?, ?, ?, ?, ?)'))
-				if($ins->bind_param('isssd', $account, $extid, $posted, $name, $amount)) {
-					$ajax->Data->count = 0;
-					$net = 0;
-					while($line = fgetcsv($fh)) {
-						// translate the data
-						$extid = $line[6];
-						$posted = date('Y-m-d', strtotime($line[1]));
-						$name = $line[7] == 'Check' ? 'Check ' . $line[5] : self::TitleCase($line[2]);
-						$amount = $line[8] == 'Debit' ? -$line[3] : +$line[3];
-						$net += $amount;
-
-						if($ins->execute())
-							$ajax->Data->count++;
-						else
-							$ajax->Fail('Error executing transaction import:  ' . $ins->error);
-					}
-					// close the statement
-					$ins->close();
-					self::UpdateAccount($account, false, $net);
-				} else
-					$ajax->Fail('Error binding import parameters:  ' . $ins->error);
-			else
-				$ajax->Fail('Database error preparing to import transactions:  ' . $db->error);
-			$db->real_query('commit');
-			fclose($fh);
-		} else
-			$ajax->Fail('Unable to open file.');
-	}
 }
 ?>
