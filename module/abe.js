a03faa37ac0eaf6d60aca205d9f14c2d5a5ff16c
misterhaan <misterhaan@users.noreply.github.com>
2019-08-14 23:07:41 -0500

    make back button work for spa

--- module/abe.js
+++ module/abe.js
@@ -1,4 +1,5 @@
 import Vue from "../external/vue.esm.browser.min.js";
+import $ from "../external/jquery-3.4.1.min.js";
 import Views from "./views.js";
 import TitleBar from "./component/titlebar.js";
 import StatusBar from "./component/statusbar.js";
@@ -23,9 +24,42 @@ new Vue({
 		}
 	},
 	created() {
-		ParseHash(this);
+		this.ParseHash();
+		$(window).on("hashchange", this.ParseHash);
 	},
 	methods: {
+		ParseHash() {
+			if(window.location.hash == "" || window.location.hash == "#")
+				this.ChangeView(Views.Home);
+			else {
+				let hash = window.location.hash.substring(1).split("!");
+				let viewPieces = hash.shift().split("/");
+				let viewName = viewPieces.shift();
+				let view = false;
+				for(let v in Views) {
+					let realView = Views[v];
+					if(realView.Name == viewName)
+						view = realView;
+				}
+				if(view) {
+					let params = false;
+					if(hash.length) {
+						params = [];
+						let paramlist = hash.join("!").split("/");
+						for(let p in paramlist) {
+							let pair = paramlist[p].split("=");
+							if(pair.length > 1)
+								params[pair.shift()] = pair.join("-");
+						}
+					}
+					if(viewPieces.length)
+						params.subview = viewPieces.join("/");
+					if(this.view != view || (params === false) != (this.params === false) || new URLSearchParams(this.params).toString() != new URLSearchParams(params).toString())
+						this.ChangeView(view, params);
+				}
+			}
+		},
+
 		ChangeView(view, params = false) {
 			this.actions = [];
 			this.params = params;
@@ -46,29 +80,3 @@ new Vue({
 		[Views.Saving.Name]: Saving
 	}
 });
-
-function ParseHash(abe) {
-	let hash = window.location.hash.substring(1).split("!");
-	let viewPieces = hash[0].split("/");
-	let viewName = viewPieces.shift();
-	let view = false;
-	for(let v in Views) {
-		let realView = Views[v];
-		if(realView.Name == viewName)
-			view = realView;
-	}
-	if(view) {
-		let params = {};
-		if(hash.length > 1) {
-			let paramlist = hash[1].split("/");
-			for(let p in paramlist) {
-				let pair = paramlist[p].split("=");
-				if(pair.length > 1)
-					params[pair.shift()] = pair.join("-");
-			}
-		}
-		if(viewPieces.length)
-			params.subview = viewPieces.join("/");
-		abe.ChangeView(view, params);
-	}
-}
