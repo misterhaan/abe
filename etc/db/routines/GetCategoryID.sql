8e934013658e8411f63e586c8bf2c8410d573938
misterhaan <misterhaan@users.noreply.github.com>
2019-08-23 08:21:01 -0500

    fix accounts links to their transactions for spa urls

--- etc/db/routines/GetCategoryID.sql
+++ etc/db/routines/GetCategoryID.sql
@@ -1,9 +1,13 @@
 create function GetCategoryID(categoryname varchar(24))
 returns tinyint unsigned
 begin
-  if categoryname='' or categoryname is null then
+  if categoryname is null then
     return null;
-  elseif (select not exists(select id from categories where name=categoryname limit 1)) then
+  end if;
+  set categoryname = trim(categoryname);
+  if categoryname = '' then
+    return null;
+  elseif (select not exists(select id from categories where name=trim(categoryname) limit 1)) then
     insert into categories (name) values (categoryname);
   end if;
   return (select id from categories where name=categoryname limit 1);
--- module/component/accounts.js
+++ module/component/accounts.js
@@ -25,7 +25,7 @@ export default {
 	mixins: [ReportErrors],
 	methods: {
 		Add() {
-			window.location = "account.php";
+			location = "account.php";
 		}
 	},
 	template: /*html*/ `
@@ -38,7 +38,7 @@ export default {
 					<span class=balance>{{account.balanceDisplay}}</span>
 				</div>
 				<div class=actions>
-					<a class=transactions :href="'transactions.php#!accts=' + account.id" title="See transactions from this account"><span>transactions</span></a>
+					<a class=transactions :href="'#transactions!accts=' + account.id" title="See transactions from this account"><span>transactions</span></a>
 					<a class=bank :href=account.bankUrl title="Visit this account’s bank website" v-if=!account.closed><span>bank</span></a>
 					<a class=import :href="'#import!acct=' + account.id" title="Import transactions to this account" v-if=!account.closed><span>import</span></a>
 					<a class=edit :href="'account.php?id=' + account.id"><span>edit</span></a>
@@ -46,6 +46,5 @@ export default {
 			</div>
 		</section>
 	`
-	// TODO: change action urls as pages get converted
 	// TODO: edit / create accounts on this page
 };
