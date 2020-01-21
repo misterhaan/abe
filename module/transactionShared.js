8d990e101cfe39146eb1fb0ca7bc1b5492233cba
misterhaan <misterhaan@track7.org>
2020-01-20 21:09:07 -0600

    increase BlurDelay (fixes #66)

--- module/transactionShared.js
+++ module/transactionShared.js
@@ -1,6 +1,6 @@
 export default {
 	UncategorizedName: "(uncategorized)",
-	BlurDelay: 100,
+	BlurDelay: 250,
 	HighlightString(str, search) {
 		var html = $("<div/>").text(str).html();
 		return search ? html.replace(new RegExp("(" + EscapeRegExp(search) + ")", "ig"), "<em>$1</em>") : html;
